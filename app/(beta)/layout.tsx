70735a222b19972b97ea5ccf35f7c4ee48ea56a6
Naibuu <hawkwer141099@gmail.com>
2024-07-28 19:39:43 +0300

    feat: beta banner

--- app/(beta)/layout.tsx
+++ app/(beta)/layout.tsx
@@ -2,5 +2,12 @@ import { Fragment, type PropsWithChildren } from 'react'
 
 // TODO: add some ui components for the beta layout
 export default function BetaLayout({ children }: PropsWithChildren) {
-    return <Fragment>{children}</Fragment>
+    return (
+        <Fragment>
+            <div className="mb-2 px-2 py-1 flex items-center justify-center text-sm text-black font-bold bg-red-500 rounded">
+                I AM STILL WORKING ON THIS, RAWHHHHH!!
+            </div>
+            {children}
+        </Fragment>
+    )
 }
